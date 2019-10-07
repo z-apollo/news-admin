cf621992b51fbe3eb3376183c11dbac4943442af
z-apollo <54058143+z-apollo@users.noreply.github.com>
2019-10-07 17:29:37 +0800

    1.项目的基本结构

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+//可以忽略，生成环境时是否有提示
 Vue.config.productionTip = false
 
+//$mount把根实例挂载到指定的节点中，相当于el
 new Vue({
   router,
   render: h => h(App)
