1b42f446d990f88366e46a923032b833ed629a9a
z-apollo <54058143+z-apollo@users.noreply.github.com>
2019-10-07 19:16:45 +0800

    4.element表单的校验

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,18 +1,18 @@
 <template>
   <el-row type="flex" justify="center" align="middle" class="container">
-
     <!-- 登录表单的div -->
     <div class="form-wrap">
       <h3>登录</h3>
 
       <!-- model：表单的数据对象 -->
-      <el-form ref="form" :model="form" label-width="80px">
-        <el-form-item label="账号">
-          <el-input v-model="form.name"></el-input>
+      <!-- rules：表单的校验规则 -->
+      <el-form ref="form" :rules="rules" :model="form" label-width="80px">
+        <el-form-item label="账号" prop="username">
+          <el-input v-model="form.username"></el-input>
         </el-form-item>
 
-        <el-form-item label="密码">
-          <el-input type="password" v-model="form.pass"></el-input>
+        <el-form-item label="密码" prop="password">
+          <el-input type="password" v-model="form.password"></el-input>
         </el-form-item>
 
         <el-form-item>
@@ -20,49 +20,65 @@
         </el-form-item>
       </el-form>
     </div>
-
   </el-row>
 </template>
 
 <script>
 export default {
-    data(){
-        return {
-            //表单的数据对象
-            form:{
-                name:"",
-                pass:""
-            }
-        }
-    },
+  data() {
+    return {
+      //表单的数据对象
+      form: {
+        username: "",
+        password: ""
+      },
 
-    methods:{
-        //提交表单
-        submitForm(){
-            console.log(this.form)
-        }
+      //rules是表单校验规则
+      rules: {
+        username: [
+          //required代表是否必填，错误时的提示，trigger什么时候触发
+          { required: true, message: "请输入用户名", trigger: "blur" }
+        ],
+        password: [
+          { required: true, message: "请输入密码", trigger: "blur" }
+        ]
+      }
+    };
+  },
+
+  methods: {
+    //提交表单
+    submitForm() {
+       // validate方法是element的表单才有的
+       this.$refs.form.validate(valid =>{
+           //valid为真时提交表单
+           if(valid){
+               //请求登录接口
+           }
+       })
     }
+  }
 };
 </script>
 
 <style scoped lang="less">
-    //在定位时同时设置top和bottom，该元素的高度取两者的距离
-    .container{
-        position: absolute;
-        top: 0;
-        bottom: 0;
-        width: 100%;
-        
-        h3{
-            text-align: center;
-            margin-bottom: 20px;
-        }
-    }
+//在定位时同时设置top和bottom，该元素的高度取两者的距离
+.container {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  width: 100%;
 
-    .form-wrap{
-        border: 1px solid #ddd;
-        padding: 20px;
-        width: 400px;
-        border-radius: 10px;
-    }
+  h3 {
+    text-align: center;
+    margin-bottom: 20px;
+  }
+}
+
+.form-wrap {
+  border: 1px solid #ddd;
+  padding: 20px;
+  width: 400px;
+  border-radius: 10px;
+}
 </style>
