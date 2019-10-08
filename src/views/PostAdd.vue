29fc0408b9f5bf279f7622810e1702ea46137448
z-apollo <54058143+z-apollo@users.noreply.github.com>
2019-10-08 21:13:00 +0800

    18.图片上传组件的使用

--- src/views/PostAdd.vue
+++ src/views/PostAdd.vue
@@ -1,17 +1,44 @@
 <template>
-  <div style="width:80%; margin-top:30px;">
-    <el-form ref="form" :model="form" label-width="80px">
+  <div style="padding:30px;">
+    <el-form :model="form" label-width="80px">
       <el-form-item label="标题">
         <el-input v-model="form.title"></el-input>
       </el-form-item>
+      
+      <!-- 富文本框 -->
 
-      <el-form-item label="内容">
-        <el-input
-        type="textarea"
-        :rows="2"
-        placeholder="请输入内容"
-        v-model="form.content">
-      </el-input>
+      <el-form-item label="栏目">
+        <!-- 栏目的数据来自于后台 -->
+        <el-checkbox-group v-model="form.categories">
+          <el-checkbox 
+          v-for="(item, index) in allCate"
+          :key="index"
+          v-if="item.id !== 999"
+          :label="item.id" 
+          name="type">{{item.name}}</el-checkbox>
+        </el-checkbox-group>
+      </el-form-item>
+
+      <el-form-item label="封面">
+        <!-- action：上传图片的链接
+        list-type: 声明当前是上传多张图片
+        on-success： 图片上传成功的函数
+        on-remove: 移除图片函数 -->
+        <el-upload
+          action="http://localhost:3000/upload"
+          name="file"
+          :headers="{
+             Authorization: token
+          }"
+          list-type="picture-card"
+          :on-success="handleSuccess"
+          :on-remove="handleRemove">
+          <i class="el-icon-plus"></i>
+        </el-upload>
+      </el-form-item>
+
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">立即创建</el-button>
       </el-form-item>
     </el-form>
   </div>
@@ -19,6 +46,7 @@
 
 <script>
 export default {
+  name: 'app',
   data(){
     return {
       // 表单的数据对象
@@ -27,9 +55,36 @@ export default {
         content: "",
         categories: [],
         cover: [],
-        type: 1
-      }
+      },
+
+      // 栏目的列表
+      allCate: [],
+      // token
+      token: JSON.parse(localStorage.getItem(`user`) || `{}`).token,
+
     }
+  },
+  methods: {
+    onSubmit(){
+      console.log(this.form.categories)
+    },
+    // 移除图片时候触发的函数
+    handleRemove(file, fileList) {
+      console.log(file, fileList);
+    },
+    // 图片上传成功的回调函数
+    handleSuccess(res, file){
+      console.log(res, file)
+    }
+  },
+  mounted(){
+    // 请求栏目的数据
+    this.$axios({
+      url: "/category"
+    }).then(res => {
+      const {data} = res.data;
+      this.allCate = data;
+    })
   }
 }
 </script>
